ff74585851ede6e7edfb01176ee0b776295247f3
alsendor <al_nucum@yahoo.com>
2018-11-02 17:45:37 -0700

    Added class to connect the databases

---
+++ app/src/main/java/com/example/alsendor/college_fitness/DatabaseAccess.java
@@ -0,0 +1,31 @@
+package com.example.alsendor.college_fitness;
+import java.sql.*;
+
+public class DatabaseAccess {
+
+    static Connection conn = null;
+
+    public static Connection connect() {
+        if (conn != null)
+            return conn;
+        try{
+            conn = DriverManager.getConnection("jdbc:sqlite:/college-fitness/src/databases/CollegeFitness.db");
+            System.out.println("Establishing connection ");
+        }
+        catch(SQLException ex) {
+            System.out.println(ex.getMessage());
+        }
+        return conn;
+    }
+
+    public static void disconnect() {
+        if (conn!= null)
+            try {
+                conn.close();
+                System.out.println("Disconnection ");
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+    }
+
+}
